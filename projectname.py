d5cb37825663ef006153523b6c72b5db0fb9fde1
finnfbhs <13211@burnside.school.nz>
2016-06-13 11:21:05 +1200

    Started estabilishing functions on code Updated database tables and columns

--- projectname.py
+++ projectname.py
@@ -1,3 +1,9 @@
 #ProjectName
 #Created by Finn Foubister 31/05/2016
 #This is a game designed for NCEA level 2 standards assessment
+def introduction():
+    print("intro text")
+
+def main():
+    introduction();
+main();
